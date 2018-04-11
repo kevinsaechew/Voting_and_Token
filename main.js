a4fe0a82a8e919bc9597b145f0b1263e4791513d
Ellen Jiang <ellenjiang@ellens-mbp-2.devices.brown.edu>
2018-04-11 09:51:32 -0400

    changing label will update model and stay stored

--- main.js
+++ main.js
@@ -6,54 +6,54 @@ var canvasWidth = canvas.width;
 var canvasHeight = canvas.height;
 var mainView = null;
 
-var lastClickedShape = null;
-var lastCommand = null;
-var last_x = null;
-var last_y = null;
-var last_radius = null;
-var last_label = null;
+var lastClickedView = null;
+//var lastCommand = null;
+
 
 
 
 var button = document.getElementById("zoom-out-button");
-button.addEventListener("click", function(event) {
-    if(mainView != null && mainView.model.parent != null) { // "mainView.model.parent != null" to prevent console error
+button.addEventListener("click", function (event) {
+    if (mainView != null && mainView.model.parent != null) { // "mainView.model.parent != null" to prevent console error
         console.log(mainView.model);
-        mainView = new NodeView(mainView.model.parent, canvasWidth/2, canvasHeight/2, 0);
+        mainView = new NodeView(mainView.model.parent, canvasWidth / 2, canvasHeight / 2, 0);
         stage.update();
     }
 }, false);
 
 // There's an add-link button that simply takes in a URL and creates a link node
 var addLinkButton = document.getElementById("add-link-button");
-addLinkButton.addEventListener("click", function(event) {
+addLinkButton.addEventListener("click", function (event) {
     var nodeName = "new link";
     var linkURL = document.getElementById('link-name').value;
     var newNode = new NodeModel(nodeName, linkURL);
     mainView.model.content.push(newNode);
-    mainView = new NodeView(mainView.model, canvasWidth/2, canvasHeight/2, 0);
+    mainView = new NodeView(mainView.model, canvasWidth / 2, canvasHeight / 2, 0);
     stage.update();
 
 });
 
 // This adds a completely new node that can be clicked 
 var addNodeButton = document.getElementById("add-node-button");
-addNodeButton.addEventListener("click", function(event) {
+addNodeButton.addEventListener("click", function (event) {
     var nodeName = document.getElementById('node-name').value;
     var linkURL = [];
     var newNode = new NodeModel(nodeName, linkURL);
     newNode.parent = mainView.model; // ^^ Maybe the parent can be added to the NodeModel constructor
     mainView.model.content.push(newNode);
-    mainView = new NodeView(mainView.model, canvasWidth/2, canvasHeight/2, 0);
+    mainView = new NodeView(mainView.model, canvasWidth / 2, canvasHeight / 2, 0);
     stage.update();
     console.log("here");
 });
 
 var editLabelButton = document.getElementById("edit-label-button");
-editLabelButton.addEventListener("click", function(event) {
+editLabelButton.addEventListener("click", function (event) {
     var newLabelName = document.getElementById('label-name').value;
     //console.log(newLabelName);
-    last_label.text = newLabelName;
+    
+    //TODO: Make a method that's UpdateLabelText() in the View, which updates the model
+    lastClickedView.model.label= newLabelName;
+    lastClickedView.label.text = newLabelName;
     stage.update();
 });
 
@@ -65,25 +65,25 @@ function init() {
 
 function load() {
     stage = new createjs.Stage("demoCanvas");
-    
+
 
     var backLabel = new createjs.Text("back", "Bold 16px Arial");
     backLabel.x = canvas.width / 2;
     backLabel.y = 50;
     backLabel.color = "#558888";
-    backLabel.set({textAlign:'center'});
+    backLabel.set({ textAlign: 'center' });
     backLabel.cursor = "pointer"
-    backLabel.addEventListener("click", function(event) {
-        if(mainView != null && mainView.model.parent != null) { // "mainView.model.parent != null" to prevent console error
+    backLabel.addEventListener("click", function (event) {
+        if (mainView != null && mainView.model.parent != null) { // "mainView.model.parent != null" to prevent console error
             console.log(mainView.model);
-            mainView = new NodeView(mainView.model.parent, canvasWidth/2, canvasHeight/2, 0);
+            mainView = new NodeView(mainView.model.parent, canvasWidth / 2, canvasHeight / 2, 0);
             stage.update();
         }
     }, false);
 
     stage.addChild(backLabel);
 
-    
+
     stage.enableMouseOver(10);
 
 }
@@ -94,10 +94,10 @@ function displayNode() {
     var last = new NodeModel("MethodJ", "https://www.methodj.com/maya-2016-female-body-modeling-tutorial/");
     testModels.push(new NodeModel("First", [last]));
 
-    
+
 
     testParentModel = new NodeModel("CG Animation", testModels);
-    mainView = new NodeView(testParentModel, canvasWidth/2, canvasHeight/2, 0);
+    mainView = new NodeView(testParentModel, canvasWidth / 2, canvasHeight / 2, 0);
 
     stage.update();
 }
@@ -110,25 +110,25 @@ class MapModel {
         this.nodes = {};
     }
 
-    addNode(){
+    addNode() {
 
 
 
     }
 
-    removeNode(){
+    removeNode() {
 
     }
 
-    groupUnderParent(){
+    groupUnderParent() {
 
     }
 
-    upvote(){
+    upvote() {
 
     }
 
-    downvote(){
+    downvote() {
 
     }
 
@@ -141,7 +141,7 @@ class NodeModel {
         this.parent = null;
         this.content = content;
 
-        if(typeof this.content != "string") {
+        if (typeof this.content != "string") {
             var i;
             for (i = 0; i < this.content.length; i++) {
                 this.content[i].parent = this;
@@ -164,7 +164,7 @@ class User {
 class NodeView {
     constructor(nodeModel, x, y, depth) {
         this.radius;
-        if(depth == 0) {
+        if (depth == 0) {
             this.radius = bgNodeRadius;
         } else {
             this.radius = nodeRadius;
@@ -174,54 +174,46 @@ class NodeView {
         this.x = x;
         this.y = y;
         this.depth = depth;
-        
+
         this.circle = new createjs.Shape(); //ehh
         this.label = new createjs.Text(this.model.label, "16px Georgia");
-       // this.command = this.circle.graphics.beginStroke("#009999").command;
+        // this.command = this.circle.graphics.beginStroke("#009999").command;
 
-        var _this = this; 
+        var _this = this;
 
         this.renderNode();
 
-        this.circle.addEventListener("click", function(event){
-          //  selectNode();
-          
-          if (lastClickedShape != null && lastClickedShape != _this.circle) {
-            //lastCommand.style = "blue";
-            lastClickedShape.graphics.clear().beginStroke("#009999").drawCircle(last_x, last_y, last_radius);
-            lastClickedShape.graphics.beginFill("#EEFFFF").drawCircle(last_x, last_y, last_radius);
+        this.circle.addEventListener("click", function (event) {
 
-          }
+            // TODO - add in ID's
+            if (lastClickedView != null && lastClickedView.circle != _this.circle) {
 
-          lastClickedShape = _this.circle;
-          last_label = _this.label;
-          last_x = _this.x;
-          last_y = _this.y;
-          last_radius = _this.radius;
-         // lastCommand = _this.command;
-        //  lastCommand.style = "yellow";
-        lastClickedShape.graphics.clear().beginStroke("#EE9999").drawCircle(_this.x, _this.y, _this.radius);
-        lastClickedShape.graphics.beginFill("#EEFFFF").drawCircle(_this.x, _this.y, _this.radius);
+
+                //displays view as deselected
+                lastClickedView.circle.graphics.clear().beginStroke("#009999").drawCircle(lastClickedView.x, lastClickedView.y, lastClickedView.radius);
+                lastClickedView.circle.graphics.beginFill("#EEFFFF").drawCircle(lastClickedView.x, lastClickedView.y, lastClickedView.radius);
+            }
+
+            lastClickedView = _this;
+
+            //displays view as selected
+            lastClickedView.circle.graphics.clear().beginStroke("#EE9999").drawCircle(_this.x, _this.y, _this.radius);
+            lastClickedView.circle.graphics.beginFill("#EEFFFF").drawCircle(_this.x, _this.y, _this.radius);
 
             stage.update();
         });
 
-        
-        if(typeof nodeModel.content != "string") {
-            
 
-            if (this.depth < 1){ //???
-                
+        if (typeof nodeModel.content != "string") {
+            if (this.depth < 1) { //???
                 this.renderContent(nodeModel.content);
             }
-
-
-            this.circle.addEventListener("dblclick", function(event){
-                mainView = new NodeView(nodeModel, canvasWidth/2, canvasHeight/2, 0);
+            this.circle.addEventListener("dblclick", function (event) {
+                mainView = new NodeView(nodeModel, canvasWidth / 2, canvasHeight / 2, 0);
                 stage.update();
             });
         } else {
-            this.circle.addEventListener("dblclick", function(event){
+            this.circle.addEventListener("dblclick", function (event) {
                 window.open(nodeModel.content);
             });
         }
@@ -232,9 +224,9 @@ class NodeView {
         //this.circle = new createjs.Shape();
         this.circle.graphics.beginFill("#EEFFFF").drawCircle(this.x, this.y, this.radius);
         this.circle.graphics.beginStroke("#009999").drawCircle(this.x, this.y, this.radius);
-        
+
         //if this is not the top-level node, show pointer cursor on hover
-        if(this.depth > 0) {
+        if (this.depth > 0) {
             this.circle.cursor = "pointer";
         }
 
@@ -242,13 +234,13 @@ class NodeView {
         this.label.x = this.x;
         this.label.y = this.y - 9;
         this.label.color = "#004444";
-        this.label.set({textAlign:'center'});
+        this.label.set({ textAlign: 'center' });
 
         stage.addChild(this.circle);
         stage.addChild(this.label);
     }
 
-    
+
     renderContent(content) {
         var rad = .6 * this.radius;
         var angle = 3 * Math.PI / 2;
@@ -264,7 +256,7 @@ class NodeView {
 
     }
 
-    updateLocation(x, y){
+    updateLocation(x, y) {
 
     }
-  }
+}
