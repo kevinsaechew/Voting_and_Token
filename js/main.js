fbb49c9ef77ce745a6ec60fd0db71a38075a32ea
Ellen Jiang <ellenjiang@ellens-mbp-2.devices.brown.edu>
2018-04-12 09:35:18 -0400

    hardcoded addresses into users

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var mainMap;
-var user;
+var mainUser;
 
 function init() {
     load();
@@ -7,15 +7,19 @@ function init() {
 
 function load() {
     // create a user
-    user = new User("0xa3hc349202913cd2f282", "Ellen");
+    mainUser = new User('0x627306090abab3a6e1400e9345bc60c78a8bef57', "Ellen");
+
+    secondUser = new User('0xf17f52151ebef6c7334fad080c5704d77216b732', "Kevin");
+
+    thirdUser = new User('0xc5fdf4076b8f3a5357c5e395ab970b5b54098fef', "Maurice");
 
     // create map model with a Modeling, and Method J node
-    var mapModel = new MapModel("CG Animation", user);
-    var modelingNM = mapModel.addNode(mapModel.root, new NodeModel("Modeling", []));
-    mapModel.addNode(modelingNM, new NodeModel("MethodJ", "https://www.methodj.com/maya-2016-female-body-modeling-tutorial/"));
+    var mapModel = new MapModel("CG Animation", mainUser);
+    var modelingNM = mapModel.addNode(mapModel.root, new NodeModel("Modeling", [], secondUser));
+    mapModel.addNode(modelingNM, new NodeModel("MethodJ", "https://www.methodj.com/maya-2016-female-body-modeling-tutorial/", thirdUser));
 
     // make a view of this model
     var stage = new createjs.Stage("demoCanvas");
     var canvas = document.getElementById('demoCanvas');
-    mainMap = new MapView(mapModel, stage, canvas, user);
+    mainMap = new MapView(mapModel, stage, canvas, mainUser);
 }
